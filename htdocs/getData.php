389677c2adb15c0260799beb625736c1f362f08f
Michi-Koenig <michael.koenig1192@gmail.com>
2024-01-22 19:34:37 +0100

    Update getData.php
    Das Json Objekt erweitert mit Daten von einem zweiten Gerät

--- htdocs/getData.php
+++ htdocs/getData.php
@@ -1,45 +1,42 @@
 <?php
+/*
 $typOfTime = $_GET['typOfTime'];
 $timeperiod = $_GET['timeperiod'];
 $projekt = $_GET['projekt'];
 $room = $_GET['room'];
-$divice = $_GET['device'];
-
-
-try {
-    $connection = new mysqli('localhost', 'root', '', 'airmonitoring');
-  } catch (Exception $e) {
-    die('MySQL-Verbindung fehlgeschlagen: ' . $e->getMessage());
-  }
-
-$sql = "SELECT * FROM users WHERE created_at >= '2015-01-01 00:00:00' AND created_at <= '2015-12-31 23:59:49'";
-foreach ($pdo->query($sql) as $row) {
-   echo $row['vorname']." ".$row['nachname']."<br />";
-   echo "E-Mail: ".$row['email']."<br /><br />";
-}
-
+$divices = $_GET['devices'];
+*/
 // Erstelle die Vriabeln
 $project = "Holderbaum";
 $room = "Schlafen";
-$device = 1;
+$devices = 2;
 
 // Erstelle die Arrays
 $arrayTime = array(1, 2, 3, 4, 5);
-$arrayTemp = array("19", "20", "20", "21", "19");
-$arrayHum = array("50", "51", "52", "51", "51");
-$arrayPres = array("965", "965", "963", "960", "961");
-$arrayCo2 = array("450", "455", "454", "455", "455");
+$arrayTemp1 = array("19", "20", "20", "21", "19");
+$arrayHum1 = array("50", "51", "52", "51", "51");
+$arrayPres1 = array("965", "965", "963", "960", "961");
+$arrayCo21 = array("450", "455", "454", "455", "455");
+
+$arrayTemp2 = array("23", "22", "21", "21", "22");
+$arrayHum2 = array("45", "47", "47", "48", "49");
+$arrayPres2 = array("954", "956", "957", "958", "955");
+$arrayCo22 = array("490", "485", "480", "480", "478");
 
 // Erstelle das JSON Objekt
 $jsonObject = array(
     "project" => $project,
     "room" => $room,
-    "device" => $device,
+    "devices" => $devices,
     "time" => $arrayTime,
-    "temp" => $arrayTemp,
-    "hum" => $arrayHum,
-    "pres" => $arrayPres,
-    "co2" => $arrayCo2
+    "temp1" => $arrayTemp1,
+    "hum1" => $arrayHum1,
+    "pres1" => $arrayPres1,
+    "co21" => $arrayCo21,
+    "temp2" => $arrayTemp2,
+    "hum2" => $arrayHum2,
+    "pres2" => $arrayPres2,
+    "co22" => $arrayCo22
 );
 
 // Gib das JSON Objekt aus
