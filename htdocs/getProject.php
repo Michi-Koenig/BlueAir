5c4a7e7a22be67cfb80ca07f17563887d164508b
Michael König <michael.koenig1192@gmail.com>
2024-05-04 14:46:58 +0200

    Create getProject.php

---
+++ htdocs/getProject.php
@@ -0,0 +1,48 @@
+<?php
+// Datenbankvariablen
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "airmonitoring";
+
+// Erstelle eine Verbindung
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Überprüfe die Verbindung
+if ($conn->connect_error) {
+  die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+// SQL-Abfrage mit DISTINCT
+$sql = "SHOW TABLES";
+
+// Führe die Abfrage aus
+$result = $conn->query($sql);
+
+// Ergebnisse in ein Array speichern
+if ($result->num_rows > 0) {
+
+  $resultArray = array();
+  while($row = $result->fetch_assoc()) {
+    $resultArray[] = $row['Tables_in_airmonitoring'];
+  }
+
+} else {
+  echo "0 Ergebnisse";
+}
+
+// Verbindung schließen
+$conn->close();
+
+// Erzeuge das JSON Objekt
+$jsonObject = array(
+  "projects" => $resultArray
+);
+
+//Gib das JSON Objekt aus
+echo json_encode($jsonObject, JSON_PRETTY_PRINT);
+?>
+
+
+  
+
